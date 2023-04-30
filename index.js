7878f8bfb62908a4bf6aab6504e37df1f9787c1a
root <root@vladybarvy.localdomain>
2023-04-30 14:24:05 +0300

    step 1

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import _ from 'lodash'
 export default function solution(content){
   // BEGIN
 
+  // step 1
+  const data = content.split('\r\n');
+  const info = data.slice(1, data.length - 1);
+  console.log(`Count: ${info.length}`);
   // END
 
 }
+
